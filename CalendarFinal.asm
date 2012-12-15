1821fb6e70f88c850bac85a1e4d752fad5f8cd0c
Irvin <irvin@Statirium.(none)>
2012-12-15 00:57:42 -0500

    modified:   CalendarFinal.asm 	modified:   README.md

--- CalendarFinal.asm
+++ CalendarFinal.asm
@@ -15,6 +15,14 @@ name "Calender"
 ; Many of the ideas for this code were scrapped together from example code in the emu8086 application
 
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; This is being added after the actual assignment!!!
+;; Damn, I wanted to be all sentimental here. This is my first public piece of code that I'm just throwing out there into the cold dark parts of the internet. I wish it all the best!!! But this will be a new footnote that is the book of my life. A footnote that people will want to flip too because fuck, who doesn't like a nice "This is where it all started" story? 
+
+;; Oh right, now I remember what I wanted to say. This is an asm program that goes ahead and takes input from the user. It looks like shit, it needs a ton of work. 
+
 org 100h
                                                                  
 ;Variables         
--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 asmCalendar
 ===========
+Damn, I wanted to be all sentimental here. This is my first public piece of code that I'm just throwing out there into the cold dark parts of the internet. I wish it all the best!!! But this will be a new footnote that is the book of my life. A footnote that people will want to flip too because fuck, who doesn't like a nice "This is where it all started" story? 
 
-Small assembly calendar that can be used to display any calendar from 2000 to 2100
+Oh right, now I remember what I wanted to say. This is an asm program that goes ahead and takes input from the user. It looks like shit, it needs a ton of work.
+
+
+All it reall does is displays a small assembly calendar that can be used to display any calendar from 2000 to 2100
